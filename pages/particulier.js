5dfb541aa77940752d91612af801d79d2fc5d2f8
Ivan <ivan.cazard@gmail.com>
2023-05-09 17:16:40 +0200

    dix commit

--- pages/particulier.js
+++ pages/particulier.js
@@ -1,13 +1,11 @@
 import Particulier from "../components/Particulier";
 import MenuParticulier from "../components/MenuParticulier";
-import FooterBleu from "../components/FooterBleu";
 
 function particulierPage() {
   return (
     <div>
       <MenuParticulier />
       <Particulier />
-      <FooterBleu />
     </div>
   );
 }
